51dd44db44453f599422cafb9a68fa682a47ec9d
Umeadi, Marlon <umeadi@gmail.com>
2019-06-11 21:04:44 -0500

    test

--- javascript/icrypto.js
+++ javascript/icrypto.js
@@ -68,9 +68,21 @@ console.log ("Group Project 1 \n Jon Palmer \n Lazontez Gardner \n Marlon Umeadi
 // Coinmarketcap
 // var cmkApiKey = "..."
 
+var cmkUrl ="https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?id=1"
+var cmkKey ="d8dea55a-8f6d-4cc5-8e53-b204e14c7e57"
 
+$.ajax({
+    beforeSend: function(xhrObj){
+        xhrObj.setRequestHeader("Accept","application/json");
+        xhrObj.setRequestHeader("X-CMC_PRO_API_KEY","d8dea55a-8f6d-4cc5-8e53-b204e14c7e5");
+        xhrObj.setRequestHeader("Access-Control-Allow-Origin","*")
+},
+    url: cmkUrl,
+    method: "GET"
+}).then(function(response) {
+    console.log(response)
 
-
+})
 
 
 
